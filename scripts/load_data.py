13ae38fe4e81912d30857ab97852b900cddb6b01
Kishan Patel <kbpatel309@gmail.com>
2025-05-25 23:58:29 -0500

    added lines to load data python file

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,2 +1,9 @@
+# pip install pandas psycopg2 sqlalchemy - install dependencies
+
 import pandas as pd
 from sqlalchemy import create_engine
+
+# Load CSV
+df = pd.read_csv('data/Hospital_General_Information.csv')
+
+# Create database connection
